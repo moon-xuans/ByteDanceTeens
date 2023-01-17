ab498e690720c362fbe429a2845ebc2eb794f29d
zhouxuan <moon_xuans@163.com>
2023-01-17 14:22:15 +0800

    增加单元测试

---
+++ advance/service/query_page_info_test.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"byteDanceTeens/advance/repository"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	repository.Init("../data/")
+	os.Exit(m.Run())
+}
+
+func TestQueryPageInfo(t *testing.T) {
+	pageInfo, _ := QueryPageInfo(1)
+	assert.NotEqual(t, nil, pageInfo)
+	assert.Equal(t, 5, len(pageInfo.PostList))
+}
